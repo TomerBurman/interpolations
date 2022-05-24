ba727d5450b38426cc4c6c64bb6381f507c35764
TomerBurman <93316511+TomerBurman@users.noreply.github.com>
2022-05-24 19:11:54 +0300

    lagrange_interpolations fully working.

---
+++ lagrange_interpolations.py
@@ -0,0 +1,31 @@
+def lagrange_interpolation(table,x):
+    '''
+    lagrange_interpolation - builds L_i polynomial for every point in the table
+    sums L_i * f(x_i) to with desired x in P(x).
+    :param table: sequence of tuples where x corresponds to f(x).
+    :param x: desired x
+    :return: P(x) value. (estimation for f(x))
+    '''
+    p = 0
+    for point in table:
+        p += build_L(table, x, point[0]) * point[1]
+    return p
+
+
+def build_L(table, x, x_i):
+    '''
+    build_L - builds Lagrange polynomial - if we have n points builds L_polynomial of degree n-1.
+    for a given point iterating through
+    all points in the table.
+    :param table: sequence of tuples where x corresponds to f(x).
+    :param x:
+    :param x_i:
+    :return: L(x) value.
+    '''
+    lagrange = 1
+    for point in table:
+        if point[0] != x_i:
+            lagrange *= (x-point[0]) / (x_i - point[0])
+    return lagrange
+
+print(lagrange_interpolation([(1, 1), (2, 0), (4, 1.5)],3))
